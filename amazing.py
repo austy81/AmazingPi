e7f6772c22539b55cd92440da7d64e95fb57f29d
Valia Antonatou <74937451+vantonatou@users.noreply.github.com>
2020-11-24 01:06:28 +0100

    Play random audio from list

--- amazing.py
+++ amazing.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from pygame import mixer
 import time
 import os
+import random
 #from os import listdir
 #from os.path import isfile, join
 
@@ -15,8 +16,7 @@ mixer.init()
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 samples_dir = os.path.join(script_path,'samples')
-samples_list = []
-wav = os.path.join(samples_dir,'test.wav')
+audio_list = []
 
 #  traverse samples directory and add all audio samples to list
 for dir_name, subdir_list, file_list in os.walk(samples_dir):
@@ -24,15 +24,18 @@ for dir_name, subdir_list, file_list in os.walk(samples_dir):
     for file_name in file_list:
         if file_name.endswith(('.wav','.mp4')):
             print('\t%s' % file_name)
-            samples_list.append(os.path.join(dir_name, file_name))
+            audio_list.append(os.path.join(dir_name, file_name))
 
-sound = mixer.Sound(wav)
+audio_file = random.choice(audio_list)
+sound = mixer.Sound(audio_file)
 
 prev_state = True
 while True:
     current_state = GPIO.input(buttonPin)
     if (current_state == False) and (prev_state == True):
         sound.stop()
-        print('Playing...')
+        audio_file = random.choice(audio_list)
+        sound = mixer.Sound(audio_file)
+        print('Playing %s' % audio_file)
         sound.play()
     prev_state = current_state
