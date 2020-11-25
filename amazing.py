832331a3da527483e43f6362586da1890e239519
Petr <petr@innit.cz>
2020-11-25 15:09:55 +0100

    Added samples and player

--- amazing.py
+++ amazing.py
@@ -1,41 +1,17 @@
 import RPi.GPIO as GPIO
-from pygame import mixer
 import time
-import os
-import random
-#from os import listdir
-#from os.path import isfile, join
+
+import player
 
 GPIO.setmode(GPIO.BOARD)
 buttonPin = 3
 
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-mixer.init()
-
-
-script_path = os.path.dirname(os.path.realpath(__file__))
-samples_dir = os.path.join(script_path,'samples')
-audio_list = []
-
-#  traverse samples directory and add all audio samples to list
-for dir_name, subdir_list, file_list in os.walk(samples_dir):
-    print('Found directory: %s' % dir_name)
-    for file_name in file_list:
-        if file_name.endswith(('.wav','.mp4')):
-            print('\t%s' % file_name)
-            audio_list.append(os.path.join(dir_name, file_name))
-
-audio_file = random.choice(audio_list)
-sound = mixer.Sound(audio_file)
-
 prev_state = True
 while True:
     current_state = GPIO.input(buttonPin)
     if (current_state == False) and (prev_state == True):
-        sound.stop()
-        audio_file = random.choice(audio_list)
-        sound = mixer.Sound(audio_file)
-        print('Playing %s' % audio_file)
-        sound.play()
+        player.play()
+    time.sleep(.1)
     prev_state = current_state
---
+++ player.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+import os
+import time
+
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+questions_dir = os.path.join(script_path, 'samples', 'questions')
+answers_dir = os.path.join(script_path, 'samples', 'answers')
+
+
+def get_random_file(dir_path):
+    audio_files = [f"{dir_path}/{f}" for f in os.listdir(dir_path) if f.endswith('.wav') or f.endswith('.mp3')]
+    return random.choice(audio_files)
+
+
+pygame.mixer.init()
+
+
+def play():
+    question = pygame.mixer.Sound(get_random_file(questions_dir))
+    answer = pygame.mixer.Sound(get_random_file(answers_dir))
+
+    question.play()
+    time.sleep(question.get_length() + 0.5)
+
+    answer.play()
+    time.sleep(answer.get_length())
