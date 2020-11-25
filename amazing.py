781f5af22b97fe8226659f3595d535a5a4f946c2
Jan Austerlitz <jaauster@microsoft.com>
2020-11-25 22:35:39 +0100

    Set warnings

--- amazing.py
+++ amazing.py
@@ -3,10 +3,9 @@ import time
 
 import player
 
-GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.ignorewarnin
 buttonPin = 3
+GPIO.setwarnings(False)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 prev_state = True
