6ec7e291f1c484e8f5896311088eb45fc4ebc3c9
Jan Austerlitz <jaauster@microsoft.com>
2020-11-25 22:53:50 +0100

    sleep 5

--- start_amazing.sh
+++ start_amazing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/pi/AmazingPi && git reset --hard && git pull && python /home/pi/AmazingPi/amazing.py
+cd /home/pi/AmazingPi && git reset --hard && sleep 5 && git pull && python /home/pi/AmazingPi/amazing.py
