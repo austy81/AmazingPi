97633a7421ec405eb2a5a6051d0512c457d04e2c
Jan Austerlitz <jaauster@microsoft.com>
2020-11-25 22:45:30 +0100

    oneliner

--- start_amazing.sh
+++ start_amazing.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Update repo
-cd /home/pi/AmazingPi && git reset --hard && git pull && chmod +x ./start_amazing.sh
-
-# Install requirements
-#pip install -r /home/pi/AmazingPi/requirements.txt
-
-# Run Amazing app
-python /home/pi/AmazingPi/amazing.py
+cd /home/pi/AmazingPi && git reset --hard && git pull && python /home/pi/AmazingPi/amazing.py
