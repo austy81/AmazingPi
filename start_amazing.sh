0b66136f0ee3cc58482ed78b380dcd6ba785a4c8
Jan Austerlitz <jaauster@microsoft.com>
2020-11-25 20:06:43 +0100

    chmod

--- start_amazing.sh
+++ start_amazing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update repo
-cd /home/pi/AmazingPi && git reset --hard && git pull
+cd /home/pi/AmazingPi && git reset --hard && git pull && chmod +x ./start_amazing.sh
 
 # Install requirements
 #pip install -r /home/pi/AmazingPi/requirements.txt
